2c61fb02dcc0fd584087508e95e0e2972336391e
Chainvisions <chainvisions@outlook.com>
2022-05-17 23:33:51 -0600

    Add Kelp minting logic

--- packages/v2/src/markets/UniswapMarketMaker.sol
+++ packages/v2/src/markets/UniswapMarketMaker.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.13;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IUniswapV2Pair} from "../interfaces/IUniswapV2Pair.sol";
+import {IERC20Ext} from "../interfaces/IERC20Ext.sol";
 import {ReentrancyGuard} from "../lib/ReentrancyGuard.sol";
 import {SafeTransferLib} from "../lib/SafeTransferLib.sol";
 import {BaseMarketMaker} from "../BaseMarketMaker.sol";
@@ -38,6 +39,7 @@ contract UniswapMarketMaker is BaseMarketMaker, ReentrancyGuard {
         // Zap into the LP.
         uint8 tokenSide = _tokenIn == _token0 ? 0 : 1;
         InternalData memory _internalData = internalData;
+        LastRecordedReserves memory _lastRecordedReserves = lastRecordedReserves;
 
         // We have to swap the token if it is not our target reserve.
         uint256 targetTokens = _amountIn;
@@ -52,7 +54,12 @@ contract UniswapMarketMaker is BaseMarketMaker, ReentrancyGuard {
 
         // Calculate and mint shares.
         // TODO: Implement math and logic for this.
-        uint256 toMint = 0;
+        uint256 __totalSupply = totalSupply();
+        uint256 totalTargetTokens = _internalData.targetBalanceOf + ((_internalData.targetReserve == 0 ? _lastRecordedReserves.reserve0 : _lastRecordedReserves.reserve1) * 2);
+        uint256 toMint = __totalSupply == 0
+            ? targetTokens
+            : (targetTokens * __totalSupply) / (totalTargetTokens);
+        _mint(msg.sender, toMint);
 
         // Create LP position.
         if(tokenSide == _internalData.targetReserve) _tokenIn.safeTransferFrom(msg.sender, address(this), _amountIn);
@@ -74,7 +81,6 @@ contract UniswapMarketMaker is BaseMarketMaker, ReentrancyGuard {
         uint256 liquiditySupply = LP_TOKEN.totalSupply();
         (uint256 reserve0, uint256 reserve1,) = IUniswapV2Pair(address(LP_TOKEN)).getReserves();
         
-        LastRecordedReserves memory _lastRecordedReserves = lastRecordedReserves;
         _lastRecordedReserves.reserve0 = uint112((reserve0 * _internalData.lpBalanceOf) / liquiditySupply);
         _lastRecordedReserves.reserve1 = uint112((reserve1 * _internalData.lpBalanceOf) / liquiditySupply);
 
@@ -100,6 +106,40 @@ contract UniswapMarketMaker is BaseMarketMaker, ReentrancyGuard {
         uint256 _tokensIn
     ) external override returns (uint256[] memory) {
         return new uint256[](0);
+    }
+
+    /// @notice Calculates how much of the target token is supplied in the Smart LP.
+    /// @return Total amount of ``TARGET_TOKEN`` held in the Smart LP position.
+    function totalSuppliedAssets() external view returns (uint256) {
+        return (
+            internalData.targetBalanceOf
+            + ((internalData.targetReserve == 0 ? lastRecordedReserves.reserve0 : lastRecordedReserves.reserve1) * 2)
+        );
+    }
+
+    /// @notice Calculates the virtual (or stored) ratio of the Smart LP.
+    /// @return The amount of `TARGET_TOKEN` one Smart LP token is worth.
+    function virtualRatio() external view returns (uint256) {
+        uint256 unit = 10 ** IERC20Ext(address(TARGET_TOKEN)).decimals();
+        uint256 _totalSuppliedAssets = internalData.targetBalanceOf
+            + ((internalData.targetReserve == 0 ? lastRecordedReserves.reserve0 : lastRecordedReserves.reserve1) * 2);
+        return totalSupply() == 0
+            ? unit
+            : (unit * _totalSuppliedAssets) / totalSupply();
+    }
+
+    /// @notice Calculates the unrealized (or current/real time) ratio of the Smart LP.
+    /// @return The amount of `TARGET_TOKEN` one Smart LP token is worth based on current pair reserves.
+    function unrealizedRatio() external view returns (uint256) {
+        uint256 unit = 10 ** IERC20Ext(address(TARGET_TOKEN)).decimals();
+        uint256 liquiditySupply = LP_TOKEN.totalSupply();
+        (uint256 reserve0, uint256 reserve1,) = IUniswapV2Pair(address(LP_TOKEN)).getReserves();
+        (uint256 uReserve0, uint256 uReserve1) = ((reserve0 * internalData.lpBalanceOf) / liquiditySupply, (reserve1 * internalData.lpBalanceOf) / liquiditySupply);
+        uint256 _totalSuppliedAssets = internalData.targetBalanceOf
+            + ((internalData.targetReserve == 0 ? uReserve0 : uReserve1) * 2);
+        return totalSupply() == 0
+            ? unit
+            : (unit * _totalSuppliedAssets) / totalSupply();
     }
 
     function _setReserveTokens() internal override {
